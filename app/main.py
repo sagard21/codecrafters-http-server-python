edb3103d41791d8d6e712ea79c5b50b578003697
Sagar Dawda <sagard21@gmail.com>
2024-05-20 22:29:19 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,17 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    while True:
+        soc, return_addr = server_socket.accept()  # wait for client
+        print(f"The return address is {return_addr}")
+        response_proto = "HTTP/1.1"
+        response_status = "200"
+        response_status_text = "OK"
+        response_message = (
+            f"{response_proto} {response_status} {response_status_text}\r\n\r\n"
+        )
+        soc.send(bytes(response_message, "utf-8"))
+        server_socket.close()
 
 
 if __name__ == "__main__":
